e18c631d5a46a49f91a5875b791599690574614b
jet <dpawsbear@gmail.com>
2021-08-16 22:59:07 +0800

    Update diy-part2.sh

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -39,6 +39,8 @@ svn co https://github.com/hubutui/p7zip-lede/trunk package/p7zip
 # add passwall
 svn co https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
 
+svn co https://github.com/xiaorouji/openwrt-passwall/tree/main/luci-app-passwall
+
 # coolsnowwolf default software package replaced with Lienol related software package
 # rm -rf feeds/packages/utils/{containerd,libnetwork,runc,tini}
 # svn co https://github.com/Lienol/openwrt-packages/trunk/utils/{containerd,libnetwork,runc,tini} feeds/packages/utils
